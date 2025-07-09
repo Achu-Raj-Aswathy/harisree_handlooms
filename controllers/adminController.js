21e61951d500075491aabc88bb055cf11cebd432
Achu-Raj-Aswathy <achurajaswathy@gmail.com>
2025-07-09 13:28:30 +0530

    product

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -161,13 +161,13 @@ const viewAddProduct = async (req, res) => {
 
 const viewListProduct = async (req, res) => {
   try {
-    const products = await Products.find();
+    const products = await Products.find().populate("categoryId");
     res.render("admin/productList", {products});
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "Internal Server error" });
   }
-};
+}
 
 const viewEditProduct = async (req, res) => {
   const productId = req.params.id;
