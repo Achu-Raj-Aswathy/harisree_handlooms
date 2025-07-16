4627ea16c5605a302b11feafef12c61cbaf38f20
Achu-Raj-Aswathy <achurajaswathy@gmail.com>
2025-07-16 13:49:43 +0530

    request model

---
+++ models/returnRequestModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const requestSchema = mongoose.Schema(
+  {
+    reqId: { type: String, unique: true },
+    orderId: { type: String, required: true },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true,
+    },
+    productName: { type: String, required: true },
+    dateOfProduct: { type: Date, required: true },
+    reason: {
+      type: String,
+      enum: ["Damaged Product", "Wrong Item Delivered", "Not as Described", "Other"],
+    },
+    image: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+requestSchema.pre("save", async function (next) {
+  if (!this.reqId) {
+    const randomNumbers = Math.floor(1000 + Math.random() * 9000); // Ensures a 4-digit number
+    this.reqId = `ReqID-${randomNumbers}`;
+  }
+});
+
+module.exports = mongoose.model("Requests", requestSchema);
